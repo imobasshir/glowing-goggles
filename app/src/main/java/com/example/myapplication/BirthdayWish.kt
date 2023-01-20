e2e2f72f99327f76272422faf52e47cb6c667fb2
Mobasshir Imam <iibrahimimam24@gmail.com>
2023-01-20 15:35:46 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/BirthdayWish.kt
@@ -0,0 +1,56 @@
+package com.example.myapplication
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+@Composable
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Bottom,
+    ) {
+        Text(
+            text = message,
+            fontSize = 28.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+        Text(
+            text = from,
+            fontSize = 21.sp,
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null, modifier = Modifier.fillMaxHeight().fillMaxWidth(), contentScale = ContentScale.Crop)
+        BirthdayGreetingWithText(message = message, from = from)
+    }
+}
+
+//@Preview
+//@Composable
+//fun BirthdayCardPreview() {
+//    MyApplicationTheme {
+//        BirthdayGreetingWithImage(message = "Happy Birthday Mobasshir!", from = " - from Ibrahim")
+//    }
+//}
